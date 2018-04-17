bc6272a5e00f6a65d08515954f1045e37121b6d1
Alex <61458673@qq.com>
2018-04-18 03:31:24 +0800

    fixed warning

--- src/component/element/Arrow.js
+++ src/component/element/Arrow.js
@@ -1,4 +1,4 @@
-import {React,connect} from 'src/common/react/Prelude';
+import {React} from 'src/common/react/Prelude';
 
 const Arrow = ({start, end, stroke="#000000"})=><path id="ProcessOnPath1010" d={"M"+start.x+" "+start.y+"L"+end.x+" "+end.y} stroke={stroke} strokeWidth="2.0" strokeDasharray="none" fill="none" markerEnd="url(#ProcessOnMarker1011)"/>
 
